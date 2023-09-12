924340ca98aca28f162c1b4fbf10002dafc618ab
Ahmed Junior fullstack developer <36606201+ahmmuh@users.noreply.github.com>
2023-09-12 09:51:14 +0200

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        MakeProduct makeProduct = new MakeProduct();
+        ProductImpl makeProduct = new MakeProduct();
         System.out.println(makeProduct.getAllProducts());
         System.out.println(makeProduct.addProduct());
     }
